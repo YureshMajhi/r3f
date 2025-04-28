3d41cf0361dccfb32eae964d36d565c7fee40d51
Yuresh <yureshmee@gmail.com>
2025-04-28 16:32:51 +0545

    animation in react fiber

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import { useRef } from "react";
+import "./App.css";
+import { Canvas, useFrame } from "@react-three/fiber";
+
+const Cube = ({ position, size, color }) => {
+  const ref = useRef();
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta;
+    ref.current.rotation.y += delta * 2;
+    ref.current.position.z = Math.sin(state.clock.elapsedTime) * 3;
+  });
+
+  return (
+    <mesh position={position} ref={ref}>
+      <boxGeometry args={size} />
+      <meshStandardMaterial color={color} />
+    </mesh>
+  );
+};
+
+const App = () => {
+  return (
+    <Canvas>
+      <directionalLight position={[0, 0, 2]} intensity={0.6} />
+      <ambientLight intensity={0.4} />
+      <Cube position={[0, 0, 0]} size={[1, 1, 1]} color={"blue"} />
+
+      {/* <group position={[1, 0, 0]}>
+        <Cube position={[1, 0, 0]} color={"orange"} size={[1, 1, 1]} />
+        <Cube position={[-1, 0, 0]} color={"pink"} size={[1, 1, 1]} />
+        <Cube position={[-1, 2, 0]} color={"yellow"} size={[1, 1, 1]} />
+        <Cube position={[1, 2, 0]} color={"red"} size={[1, 1, 1]} />
+      </group> */}
+    </Canvas>
+  );
+};
+
+export default App;
